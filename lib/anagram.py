991db614f313d99b5ac608bdb430980bb64eda9f
Christine <christinekhekama99@gmail.com>
2023-08-25 11:46:01 +0300

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self,word_list):
+        anagram_words = []
+        for word in word_list:
+            if sorted(self.word.lower()) == sorted(word.lower()):
+                anagram_words.append(word)
+        return anagram_words
